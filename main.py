fd20074948ffd46a2cdcc487e0898e649283aa49
RedstoneRadiant <76220359+TheRedstoneRadiant@users.noreply.github.com>
2023-04-06 14:41:01 -0400

    Minor refactor

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os
 import socket
 
-##############################################
-# THIS PROGRAM TAKES 9 HOURS TO FULLY EXCECUTE#
-##############################################
+################################################
+# THIS PROGRAM TAKES 9 HOURS TO FULLY EXCECUTE #
+################################################
 
 # getting local ip to use prefix
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
@@ -17,17 +17,22 @@ ipStr = str(ipArr[0]) + "." + str(ipArr[1])
 count = 0
 
 # opening file to store online hosts
-db = open("Hosts.txt", "w")
-# for loop to iterate through all possible IPs in the network
-for x in range(0, 255):
-    for y in range(0, 255):
-        # concatenating ip address
-        ip = ipStr + "." + str(x) + "." + str(y)
-        # if the ping is successful then write it to the file
-        if os.system("ping " + ip + " -n 1 -w 500") == 0:
-            db.write(ip + "\n")
-            count += 1
-        print(count)
+try:
+    db = open("Hosts.txt", "w")
+
+    # for loop to iterate through all possible IPs in the network
+    for x in range(0, 255):
+        for y in range(0, 255):
+            # concatenating ip address
+            ip = f"{ipStr}.{x}.{y}"
+
+            # if the ping is successful then write it to the file
+            if os.system(f"ping {ip} -n 1 -w 500") == 0:
+                db.write(ip + "\n")
+                count += 1
+
+            print(f"IP Addresses found: {count}")
 
 # close file
-db.close()
+finally:
+    db.close()
